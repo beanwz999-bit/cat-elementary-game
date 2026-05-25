3cfce331cf331e60d502a420e1a05aedb382a1af
Steve Wong <smwong@google.com>
2026-05-24 21:48:18 -0700

    Expose Game, MapEngine, and PuzzleRunner globally on window object for testing and console debugging

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
 // Main entry point that boots up the game engine on window load
 
 import { Game } from "./Game.js";
+import { MapEngine } from "./Map.js";
+import { PuzzleRunner } from "./PuzzleRunner.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   console.log("🐾 Booting Cat Elementary Puzzle RPG...");
   Game.init();
+  
+  // Expose key game instances globally for console testing/debugging
+  window.Game = Game;
+  window.MapEngine = MapEngine;
+  window.PuzzleRunner = PuzzleRunner;
 });
